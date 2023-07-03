e715a593a4b653030f0b840e8cef8122caf6c0b9
Arun <69082994+Arun9600@users.noreply.github.com>
2023-07-04 00:07:53 +0530

    object declaration

---
+++ Object/objectDeclaration.js
@@ -0,0 +1,20 @@
+//Method 1: Object Lieral
+let ap = { name: "Arun", age: 29 };
+
+//Method 2: Object Constructor
+let sp = new Object();
+sp.name = "Arun";
+sp.age = 29;
+
+//Access Object (Get All Properties)
+for (a in ap) {
+  console.log(a);
+}
+
+//Get All the values in an object
+for (s in sp) {
+  console.log(sp[s]);
+}
+
+//to find whether object's property is present or not
+console.log("age" in sp);
