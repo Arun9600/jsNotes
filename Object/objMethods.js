228ea63270ed10f3f5fb1afb46b4319ea8c7a80e
Arun <69082994+Arun9600@users.noreply.github.com>
2023-07-06 16:32:06 +0530

    object assign method

---
+++ Object/objMethods.js
@@ -0,0 +1,7 @@
+//Object Declaration
+let objOne = { first_name: "Arun", last_name: "prasanth" };
+let objTwo = { initial: "M", age: 28 };
+
+//Merge Two Objects
+let merge = Object.assign(objOne, objTwo);
+console.log(merge);
