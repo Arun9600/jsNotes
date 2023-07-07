042187a0a41b64ad2726666f912cbb9344b19c80
Arun <69082994+Arun9600@users.noreply.github.com>
2023-07-07 23:43:41 +0530

    basic this keyword

--- Object/objMethods.js
+++ Object/objMethods.js
@@ -5,3 +5,13 @@ let objTwo = { initial: "M", age: 28 };
 //To Merge two or objects
 let merge = Object.assign(objOne, objTwo);
 console.log(`Object Merge:`, merge);
+
+//this keyword
+let ap = {
+  name: "AP",
+  age: 28,
+  fullName: function () {
+    return this.name;
+  },
+};
+console.log(ap.fullName());
