2dc6f59f302160943070668b5e39d493174a0134
Arun <69082994+Arun9600@users.noreply.github.com>
2023-07-08 17:49:21 +0530

    Number properties

---
+++ Number/property.js
@@ -0,0 +1,20 @@
+//NUmber Property cannot be used on variables
+console.log(`Epsilon:`, Number.EPSILON);
+
+//MAX Value
+console.log(`Max Value:`, Number.MAX_VALUE);
+
+//MIN Value
+console.log(`Min Value:`, Number.MIN_VALUE);
+
+//MAX Safe Interger
+console.log(`MAX safe Interger:`, Number.MAX_SAFE_INTEGER);
+
+//Min Safe Interger
+console.log(`MIN safe Interger:`, Number.MIN_SAFE_INTEGER);
+
+//Postitive Infinity
+console.log(`Positive Infinity:`, Number.POSITIVE_INFINITY);
+
+//Negative Infinity
+console.log(`Negative Infinity:`, Number.NEGATIVE_INFINITY);
