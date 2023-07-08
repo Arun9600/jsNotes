613bffe2014eaf0aa97ff9ad8fc0e988aa3d1018
Arun <69082994+Arun9600@users.noreply.github.com>
2023-07-08 11:57:21 +0530

    string property added

---
+++ String/property.js
@@ -0,0 +1,5 @@
+//string has only one property ie. length
+//length is a numeric property it will calculate empty spaces too
+//length of the string always starts from 1
+let ap = "Arunprasanth M";
+console.log(`The length of the string is:`, ap.length);
