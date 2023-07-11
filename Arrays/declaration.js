ee12dc6dad09cf56757d3d6be6a528f927e705e3
Arun <69082994+Arun9600@users.noreply.github.com>
2023-07-11 18:45:40 +0530

    Array Declaration Added

---
+++ Arrays/declaration.js
@@ -0,0 +1,11 @@
+//Method 1:
+//Array literal and it's a more conventional method for declaring an array and it has fatser execution than normal methods
+let x = ["Arun", "Prasanth", "M"];
+//accessing array element from an array
+console.log(x[0] + " " + x[1] + " " + x[2]);
+
+//Method 2:
+//Constructor Method
+let y = new Array("Arun", "Prasanth", "M");
+//Accesing array elements from an Array
+console.log(y[0] + " " + y[1] + " " + y[2]);
